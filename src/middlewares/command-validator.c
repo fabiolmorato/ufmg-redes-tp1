ebdbe2f1fe10030dcce65b673068229c3f95d67c
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-11-23 20:01:36 -0300

    feature: command validation middleware

---
+++ src/middlewares/command-validator.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+bool command_validator_middleware(char** params, unsigned int size, char** response) {
+  for (unsigned int i = 0; i < size; i++) {
+    char* word = params[i];
+    size_t word_len = strlen(word);
+
+    for (unsigned int j = 0; j < word_len; j++) {
+      char c = word[i];
+
+      if (!isalpha(c) || !islower(c)) {
+        strncpy(*response, "invalid message", 16);
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
---
+++ src/middlewares/command-validator.h
@@ -0,0 +1,6 @@
+#ifndef COMMAND_VALIDATOR_H_INCLUDED
+#define COMMAND_VALIDATOR_H_INCLUDED
+
+bool command_validator_middleware(char** params, unsigned int size, char** response);
+
+#endif
