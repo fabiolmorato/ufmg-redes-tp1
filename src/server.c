3f4bec0ab79aa43fece28c5fa71515e7448eb735
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-11-23 17:55:02 -0300

    fix: server should receive v4/v6 as arguments, not ipv4/ipv6

--- src/server.c
+++ src/server.c
@@ -16,6 +16,12 @@ char* receive_message(char* message) {
 
 int main(int argc, char** argv) {
   validate_args(argc, argv);
+  char ip[] = "127.0.0.1";
+
+  if (strcmp(argv[1], "v6") == 0) {
+    strncpy(ip, "::1", 4);
+  }
+  
   unsigned int port = (unsigned int) atoi(argv[2]);
   socket_t socket = socket_create(argv[1], port);
   socket_listen(socket, receive_message);
@@ -23,26 +29,28 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-void usage(char** argv) {
-  fprintf(stderr, "Usage: %s <IPv4|IPv6> <PORT>\n", argv[0]);
-}
-
 void validate_args(int argc, char** argv) {
   if (argc != 3) {
-    usage(argv);
+    fprintf(stderr, "Usage: %s <v4|v6> <PORT>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (strcmp(argv[1], "v4") != 0 && strcmp(argv[1], "v6") != 0) {
+    fprintf(stderr, "Protocol must be \"v4\" or \"v6\"!\n");
+    fprintf(stderr, "Usage: %s <v4|v6> <PORT>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   if (!is_numeric_string(argv[2])) {
     fprintf(stderr, "Port must be a positive number!\n");
-    usage(argv);
+    fprintf(stderr, "Usage: %s <v4|v6> <PORT>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   unsigned int port = (unsigned int) atoi(argv[2]);
   if (port < 1024 || port > 65535) {
     fprintf(stderr, "Port is not in a valid range!\n");
-    usage(argv);
+    fprintf(stderr, "Usage: %s <v4|v6> <PORT>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 }
